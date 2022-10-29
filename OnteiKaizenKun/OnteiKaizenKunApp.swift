86dc6660d1a3493404462c5113adf327ff63672c
takuumi <satu_noone@hotmail.com>
2022-10-30 08:03:16 +0900

    Add files via upload

---
+++ OnteiKaizenKun/OnteiKaizenKunApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnteiKaizenKunApp.swift
+//  OnteiKaizenKun
+//
+//  Created by Takumi Sakamoto on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct OnteiKaizenKunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
